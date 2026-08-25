28ee3d1f202825429e8b6400d26f3b60c63bf0ef
jcausse <jcausse@itba.edu.ar>
2026-08-25 01:51:08 -0300

    [Feat] Database automatic initialization script

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -1,9 +1,13 @@
 package ar.edu.itba.paw.webapp.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -22,10 +26,13 @@ import javax.sql.DataSource;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    /* --------------------------------------------------------------- */
+    /* FRONTEND (resource management and view resolvers) */
+    /* --------------------------------------------------------------- */
+
     @Bean
     public ViewResolver viewResolver() {
         final var viewResolver = new InternalResourceViewResolver();
-
         viewResolver.setViewClass(JstlView.class);
         viewResolver.setPrefix("/WEB-INF/jsp/");
         viewResolver.setSuffix(".jsp");
@@ -37,6 +44,13 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/css/**").addResourceLocations("/css/");
     }
 
+    /* --------------------------------------------------------------- */
+    /* DATABASE (schemas, data sources, initializers and populators) */
+    /* --------------------------------------------------------------- */
+
+    @Value("classpath:schema.sql")
+    private Resource schema;
+
     @Bean
     public DataSource dataSource() {
         final var dataSource = new SimpleDriverDataSource();
@@ -45,5 +59,19 @@ public class WebConfig implements WebMvcConfigurer {
         dataSource.setUsername("postgres");
         dataSource.setPassword("postgres");
         return dataSource;
+    }
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(final DataSource ds) {
+        final var initializer = new DataSourceInitializer();
+        initializer.setDataSource(ds);
+        initializer.setDatabasePopulator(databasePopulator());
+        return initializer;
+    }
+
+    private ResourceDatabasePopulator databasePopulator() {
+        final var populator = new ResourceDatabasePopulator();
+        populator.addScript(schema);
+        return populator;
     }
 }
