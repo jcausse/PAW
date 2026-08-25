5c52d9c6a527c574b8bf996ae5fabeeb60e93f5f
jcausse <jcausse@itba.edu.ar>
2026-08-25 20:46:32 -0300

    [WIP] Initial structure to register users

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/UserForm.java
@@ -0,0 +1,22 @@
+package ar.edu.itba.paw.webapp.form;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserForm {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+
+
+
+//    private String password;              // DO NOT DELETE
+//    private String confirmPassword;       // DO NOT DELETE
+}
