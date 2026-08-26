be93ef52163e2fa9b2abf37e65be472008bcc37d
Teo Fumagalli <teo.fum@gmail.com>
2026-08-26 18:48:24 -0300

    listing create form

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ListingController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ListingController.java
@@ -5,6 +5,7 @@ import ar.edu.itba.paw.service.ListingService;
 import ar.edu.itba.paw.service.dto.ListingCreationDto;
 import ar.edu.itba.paw.webapp.form.ListingForm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
