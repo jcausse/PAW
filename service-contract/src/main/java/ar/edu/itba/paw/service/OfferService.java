f82c3ecb7916867f98e5218de21786891e090f9d
Nemo <teo.fum@gmail.com>
2026-09-11 02:43:51 -0300

    account/incoming-offers: prioritize pending offers, show resolved offers in table

--- service-contract/src/main/java/ar/edu/itba/paw/service/OfferService.java
+++ service-contract/src/main/java/ar/edu/itba/paw/service/OfferService.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.paw.service;
 
 import ar.edu.itba.paw.model.Offer;
+import ar.edu.itba.paw.service.dto.IncomingOffersDto;
 import ar.edu.itba.paw.service.dto.OfferCreationDto;
 import java.util.List;
 import java.util.Optional;
@@ -13,7 +14,7 @@ public interface OfferService {
 
     List<Offer> getByBuyerId(Long buyerId);
 
-    List<Offer> getIncomingOffersForUser(Long userId);
+    IncomingOffersDto getIncomingOffersForUser(Long userId);
 
     Offer create(OfferCreationDto dto);
 
